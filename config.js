f3d4ed286d6df0ec08fbacc2aebfbc70a44e1869
Avinash Singh <avinash03122002@gmail.com>
2023-11-02 17:19:40 +0530

    Files Added

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants={
+    openWeatherMap: {
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"64d9e65ac442b5a46e542bc67a93dde9"
+    }
+}
+module.exports=constants;
